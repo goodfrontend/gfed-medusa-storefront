dac5517a6afeeeaa986f7c7e4541a42306d89bea
Francis Eugene Casibu <projectnevz@gmail.com>
2025-12-16 16:36:44 +0800

    fix: allow external images

--- apps/mf-home/next.config.js
+++ apps/mf-home/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'medusa-public-images.s3.eu-west-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'medusa-server-testing.s3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'medusa-server-testing.s3.us-east-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
