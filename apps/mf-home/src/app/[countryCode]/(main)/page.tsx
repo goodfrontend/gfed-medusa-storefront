f07dd4910da6ccae60085978b696fdb213a32b1d
Francis Eugene Casibu <projectnevz@gmail.com>
2025-12-17 16:16:35 +0800

    test deploy

--- apps/mf-home/src/app/[countryCode]/(main)/page.tsx
+++ apps/mf-home/src/app/[countryCode]/(main)/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <div className="py-12">
+      <div className="py-10">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections as Collection[]} />
         </ul>
