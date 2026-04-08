cdba3d82908885441bfd7da2a40731d2df8a1f49
Jonathan Caburnay <jonathan@goodfrontend.dev>
2026-04-08 14:01:00 +0800

    fix: adjust skeleton loading component for order confirmation

--- packages/sf-lib-checkout/src/components/skeleton-order-items/index.tsx
+++ packages/sf-lib-checkout/src/components/skeleton-order-items/index.tsx
@@ -1,41 +1,23 @@
 const SkeletonOrderItems = () => {
+  const item = (
+    <div className="grid w-full grid-cols-[minmax(80px,_122px)_1fr] gap-x-4 overflow-hidden">
+      <div className="aspect-[29/34] w-full bg-gray-100"></div>
+      <div className="flex min-w-0 items-start justify-between gap-x-2">
+        <div className="flex min-w-0 flex-1 flex-col gap-y-2">
+          <div className="h-6 w-32 bg-gray-100 sm:w-48"></div>
+          <div className="h-4 w-24 bg-gray-100"></div>
+          <div className="h-4 w-32 bg-gray-100"></div>
+        </div>
+        <div className="h-6 w-20 shrink-0 bg-gray-100 sm:w-32"></div>
+      </div>
+    </div>
+  );
+
   return (
     <div className="flex flex-col gap-y-4 border-y border-gray-200 py-10">
-      <div className="grid grid-cols-[122px_1fr] gap-x-4">
-        <div className="aspect-[29/34] w-full bg-gray-100"></div>
-        <div className="flex items-start justify-between">
-          <div className="flex flex-col gap-y-2">
-            <div className="h-6 w-48 bg-gray-100"></div>
-            <div className="h-4 w-24 bg-gray-100"></div>
-            <div className="h-4 w-32 bg-gray-100"></div>
-          </div>
-          <div className="h-6 w-32 bg-gray-100"></div>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-[122px_1fr] gap-x-4">
-        <div className="aspect-[29/34] w-full bg-gray-100"></div>
-        <div className="flex items-start justify-between">
-          <div className="flex flex-col gap-y-2">
-            <div className="h-6 w-48 bg-gray-100"></div>
-            <div className="h-4 w-24 bg-gray-100"></div>
-            <div className="h-4 w-32 bg-gray-100"></div>
-          </div>
-          <div className="h-6 w-32 bg-gray-100"></div>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-[122px_1fr] gap-x-4">
-        <div className="aspect-[29/34] w-full bg-gray-100"></div>
-        <div className="flex items-start justify-between">
-          <div className="flex flex-col gap-y-2">
-            <div className="h-6 w-48 bg-gray-100"></div>
-            <div className="h-4 w-24 bg-gray-100"></div>
-            <div className="h-4 w-32 bg-gray-100"></div>
-          </div>
-          <div className="h-6 w-32 bg-gray-100"></div>
-        </div>
-      </div>
+      {item}
+      {item}
+      {item}
     </div>
   );
 };
